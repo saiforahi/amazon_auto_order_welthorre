eab8387ebdf65f83dc5764a6c30acfb8ca5d1db9
Shaif Azad <shaif.rahi@northsouth.edu>
2021-06-29 14:19:39 +0600

    cron removed

--- scrapper.js
+++ scrapper.js
@@ -1,5 +1,5 @@
-//const schedule = require('./plateform/amazon');
-const schedule = require('./orderSchedule')
+const schedule = require('./plateform/amazon');
+//const schedule = require('./orderSchedule')
 const scrapper = async () => {
     schedule();
 }
