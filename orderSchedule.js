431d845644447e664ca15e0ecf84242586e42a4a
Shaif Azad <shaif.rahi@northsouth.edu>
2021-06-30 20:59:26 +0600

    cron time changed

--- orderSchedule.js
+++ orderSchedule.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const amazon=require('./plateform/amazon');
 const schedule =  () => {
     console.log('cron start time-------', new Date());
-    cron.schedule('*/10 * * * *', async () => {
+    cron.schedule('*/6 * * * *', async () => {
         console.log("Schedule started.............................................................", new Date());
         Promise.all(amazon()).then(()=>{console.log('Cron stop time-------', new Date());})
         //Promise.resolve(amazon()).then(()=>{console.log('Cron stop time-------', new Date());}) 
