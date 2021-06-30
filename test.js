887592fc7333b9eae751fb416db48c6db4936e3d
Shaif Azad <shaif.rahi@northsouth.edu>
2021-06-30 17:18:04 +0600

    moment imported

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const puppeteer = require("puppeteer");
 const path = require('path');
 var userAgent = require('user-agents');
 const logger = require('./logger/logger');
+const moment = require('moment');
 var FormData = require('form-data');
 const axios = require('axios');
 const saveErrorImg = async (page) => {
